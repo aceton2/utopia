89cabf90e320d5d88ff0aa59c54f3fd24e9ada48
Andres Arguello <andres.r.arguello@gmail.com>
2019-12-22 04:17:53 +0100

    [ADD] add route listeners in logo component

--- utopia/src/app/components/logo/logo.component.ts
+++ utopia/src/app/components/logo/logo.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss']
 })
-export class LogoComponent {
+export class LogoComponent implements OnInit, OnDestroy{
 
+  routeSubscription: Subscription;
   flair: boolean | string;
+  current: boolean | string;
 
   letters = [
     { letter: 'u', route: '/idea' },
@@ -19,10 +23,22 @@ export class LogoComponent {
     { letter: 'a', route: '/contact' }
   ]
   
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.routeSubscription = this.router.events
+      .pipe(filter( event => event instanceof NavigationEnd))
+      .subscribe( (event: NavigationEnd) => this.current = this.letters.find( l => l.route === event.url).letter )
+  }
 
   logoClick(route:string) {
     this.router.navigate([route]);
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
   }
 
 }

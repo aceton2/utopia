0f595c1a8d4baf8de941ec31700668adab0c6990
Andres Arguello <andres.r.arguello@gmail.com>
2019-12-28 15:10:10 -0500

    [ADD] add island page

--- utopia/src/app/pages/island/island.component.ts
+++ utopia/src/app/pages/island/island.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-island',
   templateUrl: './island.component.html',
   styleUrls: ['./island.component.scss']
 })
-export class IslandComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class IslandComponent {
 }

9b4237d08c3b1dee777fedceae45b585062924ef
Andres Arguello <andres.r.arguello@gmail.com>
2020-01-16 22:20:13 +0100

    :sparkles: update doc page

--- utopia/src/app/pages/documentary/documentary.component.ts
+++ utopia/src/app/pages/documentary/documentary.component.ts
@@ -1,16 +1,9 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import Player from '@vimeo/player';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-documentary',
   templateUrl: './documentary.component.html',
   styleUrls: ['./documentary.component.scss']
 })
-export class DocumentaryComponent implements AfterViewInit {
-
-  @ViewChild('vimeo', {static: true}) vimeoRef: ElementRef;
-
-  ngAfterViewInit() {
-    new Player(this.vimeoRef.nativeElement, {id: "262172747"});
-  }
+export class DocumentaryComponent {
 }

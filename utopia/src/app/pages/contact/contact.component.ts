ab62582d80a1fbe8a6764f6ca1716a409ef4966c
@andres.arguello <andres.arguello@allmyhomes.com>
2020-03-08 14:08:23 +0100

    remove contact animation

--- utopia/src/app/pages/contact/contact.component.ts
+++ utopia/src/app/pages/contact/contact.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { openClose } from './animations';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss'],
-  animations: [
-    openClose
-  ],
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
 
